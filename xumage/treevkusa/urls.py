612878eb2241b9cf6f8ec5f5f04fc13f4b1ae14f
Daniil Ermolaev <blcklptn@icloud.com>
2023-05-14 18:34:13 +0500

    Index Page with css

--- xumage/treevkusa/urls.py
+++ xumage/treevkusa/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index_page, name='index_page'),
-    path('order/', views.about, name='order')
+    path('about/', views.about, name='about'),
+    path('contacts/', views.contacts, name='contacts'),
+    path('delivery/', views.delivery, name='delivery'),
+    path('login/', views.login, name='login'),
+    path('profile/', views.profile, name='profile'),
+    path('sales/', views.sales, name='sales'),
 ]
--- xumage/treevkusa/views.py
+++ xumage/treevkusa/views.py
@@ -7,4 +7,19 @@ def index_page(request):
     return render(request, 'index.html')
 
 def about(request):
-    return HttpResponse("<h1>Корзина</h1>")
+    return render(request, 'about.html')
+
+def contacts(request):
+    return render(request, 'contacts.html')
+
+def delivery(request):
+    return render(request, 'delivery.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def profile(request):
+    return render(request, 'profile.html')
+
+def sales(request):
+    return render(request, 'sales.html')
